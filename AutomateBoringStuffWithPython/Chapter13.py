bd50774698a9e945a79782060f48e734630903cf
ssing903 <ssing902@my.centennialcollege.ca>
2019-09-04 13:23:59 -0400

    added some changes

---
+++ AutomateBoringStuffWithPython/Chapter13.py
@@ -0,0 +1,13 @@
+# Chapter 13 - working with pdf and stuff 
+
+import os,PyPDF2
+
+'''
+pdfFileObj = open('meetingminutes.pdf','rb')
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+print(pdfReader.getPage(0).extractText())
+'''
+
+# Decrypting PDF
+os.chdir('C:\\Users\\Sukh\\Desktop\\automate_online-materials')
+
